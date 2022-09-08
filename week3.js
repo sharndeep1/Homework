e428e731e33a339ab5afd9fb02e35b264e0572d9
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-09-08 09:38:02 -0500

    modified

--- week3.js
+++ week3.js
@@ -1,56 +1,73 @@
 //Create an array of pizzaToppings with at least four different toppings
-let pizzaToppings =['Onion',"Bell","peppers","Corn","Mushroom"];
+const pizzaToppings =["onion","bell","peppers","corn","mushroom"];
 
 //Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c", see Bonus Challenge #9)
 //i.e. "Welcome to Pizza House, our toppings are: a, b, c, ..."
 
 function greetCustomer() {
-  //for (let i = 0; i <  pizzaToppings.length ; i++) {
-    console.log("Welcome to Pizza House, our toppings are:",pizzaToppings);
+  let greeting ="Welcome to Pizza House, our toppings are:";
+  for (let topping of pizzaToppings) {
+    greeting += `${topping}`;
   }
-//}
+  console.log(greeting);
+}
 greetCustomer();
 
 //Create a getPizzaOrder function that
 //has the parameters size, crust, and an indefinite amount of toppings as inputs
 //prints the order, i.e. "One large thick crust pizza with x, y, z, ... coming up!"
 //outputs a list with the size, crust, and toppings
-let pizzaOrder;
-function getPizzaOrder(size,c, ...indefAmtToppings) {
+
+function getPizzaOrder(size,crust, ...toppings) {
+  let pizzaOrder = `one ${size} , ${crust} pizza with`;
+  for (let topping of toppings){
+    pizzaOrder +=`${topping}`;
+}
+console.log(`${pizzaOrder} coming up!`);
 
 
-    pizzaOrder = console.log("one", size , c," pizza with", indefAmtToppings, "coming up!");
 
-  return   pizzaOrder;
+
+return [size,crust,toppings];
 }
 
-
-
-getPizzaOrder("large","thick crust","onions","green peppers","olive");
+ let customerOrder = getPizzaOrder("large","thick crust","onions","green peppers","olive");
 
 //Create a preparePizza function that has an array as its parameter with three items: a size, a crust, and a list of toppings
 //prints something like "...Cooking pizza..."outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings
-let pizza = {
-  size: "large",
-  crusts: "thick",
-  listOfToppings:["onions","peppers","spinach"]
-};
- let prepareThePizza;
-function preparePizza(pizza) {
 
-prepareThePizza = console.log("....cooking pizza ... one", pizza.size,pizza.crusts,pizza.listOfToppings);
-return  prepareThePizza;
+function preparePizza([orderSize,orderCrust,orderToppings]) {
+
+
+ console.log("....cooking pizza ... one");
+
+return {
+  size: orderSize,
+  crust: orderCrust,
+  toppings: orderToppings
+};
 }
 
-preparePizza(pizza);
+ let cookPizza = preparePizza(customerOrder);
 
 //Create a servePizza function that has a parameter of a pizza Object logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!"
 //outputs the same pizza Object that was passed in
-let serve;
+
 function  servePizza(pizza){
-  serve = console.log(`Order up! ${pizza["size"]} ${pizza["crusts"]} crust  pizza with ${pizza["listOfToppings"]} Enjoy!`);
- return serve;
+  let serve = `Order up! ${pizza.size} ${pizza.crust} crust  pizza with  `;
+  for(let topping of pizza.toppings){
+    serve += `${topping}, `;
+  }
+  console.log(`${serve} coming up!`);
+
+ return pizza;
 
 
 }
-servePizza(pizza);
+servePizza(cookPizza);
+
+greetCustomer();
+const myPizza = servePizza(
+  preparePizza(getPizzaOrder("large","pan","onions","greenPepper"))
+);
+console.log(myPizza);
