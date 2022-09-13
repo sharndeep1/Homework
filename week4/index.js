7d8024d25d5d1679d8f485b4762d6495e24c7b23
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-09-13 17:10:33 -0500

    finished week4 homework

--- week4/index.js
+++ week4/index.js
@@ -3,3 +3,4 @@
 document.querySelector("main > p").style.cssText =
   "color: yellow ; border: 1px solid black";
 
+  document.querySelector("#image").addEventListener("click", () => alert("You clicked the picture"));
