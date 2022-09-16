0157656e70b7eee85f1e3befe7b9d9f03f933454
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-09-15 21:52:01 -0500

    update

--- week4/index.js
+++ week4/index.js
@@ -1,6 +1,5 @@
 
 
-document.querySelector("main > p").style.cssText =
-  "color: yellow ; border: 1px solid black";
 
-  document.querySelector("#image").addEventListener("click", () => alert("You clicked the picture"));
+
+document.getElementById("#image").addEventListener("click", () => alert("You clicked the picture"));
